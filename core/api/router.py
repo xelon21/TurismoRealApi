bfb02550675ed05198c912ae71e4aeeb15ddb09e
Marcelo MVV. Valenzuela V <mvalenzuela@incomerltda.cl>
2022-11-24 17:07:14 -0300

    aaa

---
+++ core/api/router.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import DefaultRouter
+# from core.api.views import PostApiViewSet
+
+# router_post = DefaultRouter()
+
+# router_post.register(prefix='post', basename='post', viewset=PostApiViewSet)
---
+++ core/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from core.models import Zona, Departamento
+
+#class PostSerializer(ModelSerializer):
+   # class Meta:
+     #   model = Zona
+    #    fields = ['id_zona', 'descripcion']
+
+
+#class DepartamentoSerializer(serializers.ModelSerializer):
+ #   class Meta:
+  #      model = Departamento
+   #     fields = '__all__'
